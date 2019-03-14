1f4b974e77764f64651d34acd22d5ece1aafaffa
aguestuser <aguestuser@riseup.net>
2019-03-13 20:56:49 -0400

    [32] add e2e tests for `channels#initialize`
    side-effects:
    * add nicer logging for `channels#initialize`
    * introduce separate docker-compose files for unit, e2e tests

--- app/services/orchestrator/channel/activate.js
+++ app/services/orchestrator/channel/activate.js
@@ -1,3 +1,5 @@
+import { prettyPrint } from '../../util'
+
 const docker = require('../docker')
 const channelRepository = require('../../../db/repositories/channel')
 const phoneNumberRepository = require('../../../db/repositories/phoneNumber')
@@ -24,7 +26,9 @@ const activate = async ({ db, phoneNumber, name, admins }) => {
     await channelRepository.updateOrCreate(db, phoneNumber, name, containerId)
     await channelRepository.addAdmins(db, phoneNumber, admins)
     return {
-      ...(await phoneNumberRepository.update(db, phoneNumber, { status: statuses.ACTIVE })),
+      ...(await phoneNumberRepository
+        .update(db, phoneNumber, { status: statuses.ACTIVE })
+        .then(x => x.dataValues)),
       name,
       admins,
     }
