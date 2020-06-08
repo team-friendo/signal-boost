c133308ba52878b32f64c9e7dcb9aa2212c48e8f
Jake Miles <jacob.miles@gmail.com>
2020-06-08 02:42:21 -0400

    fixed server startup - was missing import

--- app/services/dispatcher/routes.js
+++ app/services/dispatcher/routes.js
@@ -1,3 +1,5 @@
+const { prometheusMetricsRoute } = require('../prometheus_metrics_route')
+
 const routesOf = async (router, metrics) => {
   router.get('/metrics', prometheusMetricsRoute(metrics))
 }
