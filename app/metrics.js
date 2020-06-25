c1824652581f6406307b7e7a02dc1d941eba37a7
fdbk <fdbk@riseup.net>
2020-06-24 21:56:49 -0400

    [273] Add relayable messages count monitor.

--- app/metrics.js
+++ app/metrics.js
@@ -1,14 +1,27 @@
 const prometheus = require('prom-client')
+const app = require('./index')
 
 const register = (registry, metric) => ({ ...metric, registers: [registry] })
 
 const run = () => {
   const registry = new prometheus.Registry()
   prometheus.collectDefaultMetrics({ registry })
-  return registry
+
+  const relayableMessageCounter = new prometheus.Counter({
+    name: 'relayable_messages',
+    help: 'Counts the number of relayed messages',
+    registers: [registry],
+    labelNames: ['channelPhoneNumber'],
+  })
+
+  return { registry, relayableMessageCounter }
 }
+
+const incrementRelayableMessageCounter = channel =>
+  app.metrics.relayableMessageCounter.labels(channel.phoneNumber).inc()
 
 module.exports = {
   run,
   register,
+  incrementRelayableMessageCounter,
 }
